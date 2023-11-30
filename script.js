20a3f23cd654750f0b84e5791c96400d9f3b2cd1
danokirror@gmail.com <danokirror@gmail.com>
2023-11-30 18:35:00 +0300

    started working on the functionality of the buttons

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-document.addEventListener('keydown', (event) => {
-    if( event.key == "0"){document.body.append("0")}
-    else if(event.key == "1"){document.body.append("1")}
-    else if(event.key == "2"){document.body.append("2")}
-    else if(event.key == "3"){document.body.append("3")}
-    else if(event.key == "4"){document.body.append("4")}
-    else if(event.key == "5"){document.body.append("5")}
-    else if(event.key == "6"){document.body.append("6")}
-    else if(event.key == "7"){document.body.append("7")}
-    else if(event.key == "8"){document.body.append("8")}
-    else if(event.key == "9"){document.body.append("9")}
-    else if(event.key == "/"){document.body.append("/")}
-    else if(event.key == "*"){document.body.append("*")}
-    else if(event.key == "-"){document.body.append("-")}
-    else if(event.key == "+"){document.body.append("+")}
-});
+document.getElementById('1').addEventListener('click', input);
+document.getElementById('2').addEventListener('click', input);
+document.getElementById('3').addEventListener('click', input);
+document.getElementById('4').addEventListener('click', input);
+document.getElementById('5').addEventListener('click', input);
+document.getElementById('6').addEventListener('click', input);
+document.getElementById('7').addEventListener('click', input);
+document.getElementById('8').addEventListener('click', input);
+document.getElementById('9').addEventListener('click', input);
+document.getElementById('0').addEventListener('click', input);
+document.getElementById('+').addEventListener('click', input);
+document.getElementById('-').addEventListener('click', input);
+document.getElementById('*').addEventListener('click', input);
+document.getElementById('/').addEventListener('click', input);
+document.getElementById('%').addEventListener('click', input);
+document.getElementById('delete').addEventListener('click', del);
+document.getElementById('c').addEventListener('click', clear);
+document.getElementById('ac').addEventListener('click', reset);
+document.getElementById('equals').addEventListener('click', equals);
