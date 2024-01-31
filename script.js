7165aaf439aab9af52705a3ccb10d5106dff5886
danokirror@gmail.com <danokirror@gmail.com>
2024-01-31 15:56:21 +0300

    added a function that deals with nmbers and operators

--- script.js
+++ script.js
@@ -37,5 +37,38 @@ function reset() {
 }
 
 function equals(){
+  var inputString = display.innerText;
+  var numbers = inputString.split(/\+|\-|\*|\//g);
+  var operators = inputString.split(/[0-9]|\./g,"").split("");
 
-      }
+  var divide = operators.indexOf("/");
+  while(divide !=-1){
+    numbers.splice(divide,2,numbers[divide]/numbers[divide + 1]);
+    operators.splice(divide,1);
+    divide= operators.indexOf("/");
+  }
+
+  var multiply = operators.indexOf("*");
+  while(multiply !=-1){
+    numbers.splice(multiply,2,numbers[multiply]*numbers[multiply + 1]);
+    operators.splice(multiply,1);
+    multiply= operators.indexOf("*");
+  }
+
+  var subtract = operators.indexOf("-");
+  while(subtract !=-1){
+    numbers.splice(subtract,2,numbers[subtract]-numbers[subtract + 1]);
+    operators.splice(subtract,1);
+    subtract= operators.indexOf("-");
+  }
+
+  var add = operators.indexOf("+");
+  while(add !=-1){
+    numbers.splice(add,2,parseFloat(numbers[add])+ parseFloat(numbers[add + 1]));
+    operators.splice(add,1);
+    add= operators.indexOf("+");
+  }
+
+  displayCurrent= numbers[0];
+  display.innerText= displayCurrent;
+}
